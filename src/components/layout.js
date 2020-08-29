be900c742b3a114160bcdfab835d2193d03bbc43
csisoap <hoaianro@gmail.com>
2020-08-29 13:43:40 +0700

    Add build command

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import { IdentityContextProvider } from "react-netlify-identity-widget";
 import "./layout.css";
 
 const Layout = ({ children }) => (
-  <IdentityContextProvider url="https://jamstack-auth.netlify.com">
+  <IdentityContextProvider url="https://my-jamstack-app.netlify.app">
     <header>
       <Link to="/">JAMstack App</Link>
     </header>
